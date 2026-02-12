dba5886ecb895f401732be3f07cd88a75e86145c
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2026-02-12 19:17:27 +0000

    minor debugging

--- module.py
+++ module.py
@@ -1,4 +1,20 @@
 class Robot:
     def __init__(self, name, color):
         # initialize properties code here
-        pass
+        self.battery = int(100)
+        self.durability = ()
+        self.productivity = ()
+        self.work = int()
+        pass    
+    
+    def charge(self, amount):
+        self.battery += int(amount)
+        return self.charge 
+    
+    def work(self, amount):
+        int(hours)
+        self.battery -= hours * 10
+        self.durability -= hours * 5
+        self.productivity += hours * 8
+        return f"{self.name} works for {hours} hours."
+    #if  repair(self):
